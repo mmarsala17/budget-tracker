eded227b27628a66f797f496be932d5e5d0e132d
Mark Marsala <mmarsala76@gmail.com>
2022-01-09 23:43:59 -0500

    transaction data

---
+++ develop/public/js/idb.js
@@ -0,0 +1,32 @@
+let db;
+
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+
+    const db = event.target.results;
+
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+
+    db = event.target.results;
+
+    if (navigator.onLine) {
+
+    }
+};
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    budgetObjectStore.add(record);
+};
