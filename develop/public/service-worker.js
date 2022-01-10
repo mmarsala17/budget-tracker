c3a5d1221d6ccff73b0b07487521955966d25b79
Mark Marsala <mmarsala76@gmail.com>
2022-01-10 00:19:19 -0500

    service worker activation

--- develop/public/service-worker.js
+++ develop/public/service-worker.js
@@ -17,3 +17,22 @@ self.addEventListener('install', function (e) {
         })
     )
 });
+
+self.addEventListener('activate', function (e) {
+    e.waitUntill(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+});
